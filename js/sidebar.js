1730ad40094bb3fcf7de7d3787e49fe4fbd447c6
noahrmitchell <noahrmitchell@gmail.com>
2021-03-25 22:17:29 +0000

    Started adding transitions

---
+++ js/sidebar.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "30vw";
+    document.getElementById("page-wrap").style.margin-left = "30vw";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("page-wrap").style.margin-left = "0";
+  } 
